dfaec7c63d9b30226f90d1f595b80e12fef49a6e
Kate <hello.kategrant@gmail.com>
2021-12-04 22:53:12 -0500

    initial commit

---
+++ AdventAnswers.fs
@@ -0,0 +1,19 @@
+﻿open System
+open System.IO
+open Day1.Part1
+open Day1.Part2
+
+[<EntryPoint>]
+let main argv =
+    printfn "Day 1";
+    let sonarData = dataReader "Day1PuzzleInput.txt" in
+    printfn "Day 1 Data: %A" sonarData;
+    printfn "Part 1:"
+    let D1P1Solution = Day1.Part1.increaseTotaller sonarData Day1.Part1.yIsGreater;
+    printfn "%d is the solution" D1P1Solution.increaseCount
+    printfn "Part 2:"
+    let D1P2Solution = Day1.Part2.increaseTotaller sonarData Day1.Part2.yIsGreater;
+    printfn "%d is the solution" D1P2Solution.increaseCount
+    printfn "-------------------------------------------"
+    
+    0 // return an integer exit code
---
+++ Day1Part2.fs
@@ -0,0 +1,33 @@
+﻿namespace Day1
+module Part2 =
+
+    open System
+    open System.IO
+
+    type public changeAcc = {mutable indexCount: int; mutable increaseCount: int; arr: array<int>}
+
+    let dataReader (filePath : string) =
+        new StreamReader(Path.Join(__SOURCE_DIRECTORY__, filePath)) |> Array.unfold (fun strmrd -> 
+        match strmrd.ReadLine() with
+        | null -> strmrd.Dispose(); None 
+        | str -> Some(str |> int, strmrd);)
+
+    let yIsGreater (x : changeAcc) (y : int) : changeAcc =
+        if x.indexCount < (Array.length x.arr) - 3
+        then
+            let mutable i = x.indexCount;
+            let mutable ic = x.increaseCount;
+            let groupA = (y + x.arr.[x.indexCount + 1] + x.arr.[x.indexCount + 2]);
+            let groupB = (x.arr.[x.indexCount + 1] + x.arr.[x.indexCount + 2] + x.arr.[x.indexCount + 3]);
+            let difference = groupA - groupB in
+            match difference with
+            | _ when difference < 0 -> {indexCount = i + 1; increaseCount = ic + 1; arr = x.arr};
+            | _ when difference >= 0 -> {indexCount = i + 1; increaseCount = ic + 0; arr = x.arr};
+            |_ -> failwith "match error";
+            else x
+ 
+    let increaseTotaller (dataArr : array<int>) f =
+        let mutable acc = {indexCount = 0; increaseCount = 0; arr = dataArr};
+        Array.fold (fun x y -> f x y) acc dataArr;
+
+       
